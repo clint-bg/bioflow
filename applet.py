7f87e1e049bef4deb16909d5aae4c49a192d2b9f
Clint <b.guymon@gmail.com>
2025-12-21 18:34:56 -0700

    Add section header 'hello' to applet.py
    test

--- applet.py
+++ applet.py
@@ -6,6 +6,8 @@ import altair as alt
 st.title('bioflow model')
 st.markdown('Bioreactor code that models the concentration of NPEC (non-pathogenic E. coli) that produces GFP (green fluorescent protein) in a bioreactor. It also models the concentration of oxygen and the mass transfer rate. The mass transfer rate increases with agitation and flow rate of air into the bioreactor.')
 
+st.markdown('## hello')
+
 # Sidebar
 st.sidebar.title('parameters')
 st.sidebar.markdown('Adjustable parameters.')
