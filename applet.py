5ee70c7bb15d9cb5249ab2f8a39a2d7fd6dc0508
Clint <b.guymon@gmail.com>
2025-12-21 18:37:35 -0700

    Enhance model description with Logistic-Monod details
    Added explanation of the Logistic-Monod growth model to the applet.

--- applet.py
+++ applet.py
@@ -6,7 +6,15 @@ import altair as alt
 st.title('bioflow model')
 st.markdown('Bioreactor code that models the concentration of NPEC (non-pathogenic E. coli) that produces GFP (green fluorescent protein) in a bioreactor. It also models the concentration of oxygen and the mass transfer rate. The mass transfer rate increases with agitation and flow rate of air into the bioreactor.')
 
-st.markdown('## hello')
+st.markdown('## Understand the Logistic-Monod Growth Model  
+
+The logistic portion of the growth model models the limits of growth due to the carrying capacity of the bioreactor. The Monod portion models the growth rate as a function of the substrate concentration. The logistic-Monod growth model can be expressed as (Monod portion first and the logistic portion second):
+
+$$
+\frac{1}{X}\frac{dX}{dt} = \left[ \mu_a + \frac{\mu_{m}S}{K_s + S} \right] \left[ 1-\frac{X}{X_m} \right]
+$$
+
+')
 
 # Sidebar
 st.sidebar.title('parameters')
