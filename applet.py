3e7ee2b161aed85219a7df4795cc0b1cfb07ddaf
Clint <b.guymon@gmail.com>
2025-12-21 18:31:51 -0700

    Create Streamlit app for bioflow model
    Initial implementation of a Streamlit app to model bioreactor dynamics.

---
+++ applet.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+st.title('bioflow model')
+st.markdown('Bioreactor code that models the concentration of NPEC (non-pathogenic E. coli) that produces GFP (green fluorescent protein) in a bioreactor. It also models the concentration of oxygen and the mass transfer rate. The mass transfer rate increases with agitation and flow rate of air into the bioreactor.')
+
+# Sidebar
+st.sidebar.title('parameters')
+st.sidebar.markdown('Adjustable parameters.')
+
